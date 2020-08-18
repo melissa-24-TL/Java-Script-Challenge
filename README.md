881d01fd36001b224b1e2d579c1c3147f8fbbdca
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-08-18 14:37:21 -0400

    created a challenge

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Java-Script-Challenge
 A JS Challenge
+
+
+## index.js
+
+In the index.js file add code to answer the questions.
---
+++ index.js
@@ -0,0 +1,47 @@
+const cakeDonut = {
+	"id": "0001",
+	"type": "donut",
+	"name": "Cake",
+	"ppu": 0.55,
+	"batters": {
+			"batter":
+				[
+					{ "id": "1001", "type": "Regular" },
+					{ "id": "1002", "type": "Chocolate" },
+					{ "id": "1003", "type": "Blueberry" },
+					{ "id": "1004", "type": "Devil's Food" }
+				]
+		},
+	"topping": [
+			{ "id": "5001", "type": "None" },
+			{ "id": "5002", "type": "Glazed" },
+			{ "id": "5005", "type": "Sugar" },
+			{ "id": "5007", "type": "Powdered Sugar" },
+			{ "id": "5006", "type": "Chocolate with Sprinkles" },
+			{ "id": "5003", "type": "Chocolate" },
+			{ "id": "5004", "type": "Maple" }
+		]
+}
+
+//CHALLENGE
+// Console.log() --
+// 1. "Sugar"
+// 2. Length of "batter"
+// 3. 5th "id" in "topping"
+// 4. An array of the different toppings, minus "Sugar"
+
+// 1. "Sugar"
+console.log(/* code here*/);
+
+// 2. Length of "batter"
+console.log(/* code here*/);
+
+// 3. 5th "id" in "topping"
+console.log(/* code here*/);
+
+// 4. An array of the different toppings, minus "Sugar"
+
+/* code here*/
+console.log();
+
+// alt  #4
